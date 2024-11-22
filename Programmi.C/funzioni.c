2bbdb65751d5fc14f914c838dd143d0d51f772cf
Ahmed74d <abdelfatahelshourbgy@gmail.com>
2024-11-22 11:59:40 +0000

    g

--- Programmi.C/funzioni.c
+++ Programmi.C/funzioni.c
@@ -42,32 +42,218 @@ int prodotto(int a, int b )
 */
 
 // funzione che verifica se un numero è pari 
-#include <stdio.h> 
-#define N 50 
+/*#include <stdio.h> 
+#define N 10
 
-int numeropari(int v[]);
+int nPari(int v[]);
 
 int main ()
 {
     int vet[N];
-    int a = numeropari(vet);
+    int i;
+    
+    printf("inserisci i numeri che vuoi analizzare: \n");
+    for(i= 0; i < N ; i++)
+    {
+        printf("numer %d\n", i +1 );
+        scanf("%d", &vet[i]);
+    }
+
+    int a = nPari(vet);
     printf("i numeri pari sono %d \n", a);
-    return 0;
+    return 0; 
 }
 
-int numeroPari(int v[])
+int nPari(int v[])
 {
-    int i = 0;
-    for(i = 0 ; i < N ; i++)
+    int count = 0; 
+    for(int i = 0 ; i < N ; i++)
     {
         if(v[i] % 2 == 0)
         {
-            return i ;
+            count ++;
         }
-        else 
+    }
+    return count;
+}
+*/
+/*#include <stdio.h>
+
+int sPari(int i );
+
+int main ()
+{
+    int a; 
+    printf("inserisci il numero che ti serve: \n");
+    scanf("%d", &a);
+
+    int f = sPari(a); 
+    if (f)
+    {
+        printf("il numero %d e' pari\n", f);
+    }
+    else 
+    {
+        printf("il numero %d e' dipari\n", f);
+    }
+    
+
+    return 0 ; 
+
+}
+
+int sPari(int i )
+{
+    if (i % 2 == 0)
+    {
+        printf("%d\n",i); 
+        return i ; 
+    }
+   
+     else 
+    {
+        return 0 ; 
+    }
+    
+    
+}
+*/  
+
+// trovare il maggiore tra 2 numeri 
+
+/*#include <stdio.h>
+int Maggiore(int a , int b );
+
+int main ()
+{
+    int m , n;
+    printf("inserisci il primo numero : \n");
+    scanf("%d", & m);
+    printf("inserisci il secondo numero : \n");
+    scanf("%d", & n);
+
+    int f = Maggiore (m , n);
+    printf("il numero maggiore e' %d\n", f);
+
+    return 0 ; 
+    
+}
+
+
+int Maggiore(int a , int b )
+{
+    if (a > b )
+    { 
+        return a ; 
+    }
+    else 
+    {
+        return b ; 
+    }
+}
+*/
+
+// calcolare il fattoriale di un numero 
+/*#include <stdio.h>
+int Fattoriale(int N);
+
+int main ()
+{
+    int f; 
+    printf("inserisci il numero : \n");
+    scanf("%d", & f);
+
+    int m = Fattoriale(f);
+    printf("il fattorale del numero è %d \n", m);
+
+    return 0;
+}
+
+int Fattoriale (int N)
+{ 
+    int fat=1; 
+    for(int i = 1 ; i <= N ; i++)
+    {
+        fat = fat * i ;
+    }
+
+     return fat;
+}
+*/
+
+// calcola la somma di un array 
+
+/*#include <stdio.h>
+#define N 5 
+int Somma(int v[]);
+
+int main()
+{
+    int vet[N];
+    int i;
+    printf("inserisci un insieme di numeri max g : \n");
+    for(i = 0; i < N ; i++)
+    {
+        printf("il numero : %d\n", i+1);
+        scanf("%d", &vet[i]);
+    }
+    int f = Somma(vet);
+    printf("%d\n", f);
+
+    return 0 ; 
+}
+
+int Somma(int v[])
+{
+    
+    int count = 0 ; 
+    for(int i =0; i < N; i++)
+    {
+       count = count + v[i]; 
+    } 
+    
+    //  non serve il count ++ pk tanto il conto si fa con il count che cambia ogni volta
+    return count; 
+}
+*/
+
+// funzione che inverte un array
+
+#include <stdio.h>
+void invertiA(int v[], int dim);
+
+int main ()
+{
+    int vet[] = {1,2,3,4,5};
+    int dim = sizeof(vet) / sizeof(vet[0]); 
+    printf("stampa l'array originale \n");
+    for(int i = 0; i < dim ; i++)
+    {
+        printf("%d\n", vet[i]);
+    }
+
+    invertiA(vet, dim);
+        printf("array invertito \n");
+        for(int i = 0; i < dim ; i++)
         {
-             return 0 ; 
+            printf("%d", vet[i]);
         }
-       
+        printf("\n");
+
+    return 0 ; 
+
+
+
+}
+
+void invertiA(int v[], int dim)
+{
+    int temp ; 
+    for (int i = 0; i < dim / 2 ; i++)
+    {
+        temp = v[i];
+        v[i] = v[dim - 1 - i];
+        v[dim - 1 - i] = temp ; 
+
     }
 }
